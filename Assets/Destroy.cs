915b6cfceab5399542ec8371561114aa5be188a4
渡部かすみ <kasumisheep@gmail.com>
2017-04-29 21:01:00 +0900

    StepAsideUnityChanの課題
    不要になったアイテムを破棄・アイテムを生成するスクリプトを追加

---
+++ Assets/Destroy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+    //carPrefabを入れる
+    public GameObject carPrefab;
+    //coinPrefabを入れる
+    public GameObject coinPrefab;
+    //cornPrefabを入れる
+    public GameObject conePrefab;
+    //スタート地点
+    private int startPos = -160;
+    //ゴール地点
+    private int goalPos = 120;
+    //アイテムを出すx方向の範囲
+    private float posRange = 3.4f;
+
+
+    private GameObject unitychan;//
+
+    // Use this for initialization
+    void Start () {
+
+        this.unitychan = GameObject.Find("unitychan");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //１つ１つにアタッチ
+        if (this.transform.position.z < unitychan.transform.position.z-20)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+}
